4d6a8b257b62ab82f0a5e83043c044f9fa5504e0
Jai <loveyhtu21@gmail.com>
2016-05-20 21:27:54 +0530

    make it better

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,6 +39,9 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
   config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.default_url_options = { :host => 'http://45.58.47.179', port: 3001 }
+
      
 ActionMailer::Base.smtp_settings = {
   :user_name => 'fun.all.here@gmail.com',
