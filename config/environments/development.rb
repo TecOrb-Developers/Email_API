300b94c0c23880a66c9681bfb3478aa9f4e03764
Jai <loveyhtu21@gmail.com>
2016-05-24 22:49:27 +0530

    make it better

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,8 +41,8 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
   config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.default_url_options = { :host => 'http://localhost:3000' }
-  # config.action_mailer.default_url_options = { :host => 'http://45.58.47.179', port: 3001 }
+  # config.action_mailer.default_url_options = { :host => 'http://localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'http://45.58.47.179', port: 3001 }
   
      
 ActionMailer::Base.smtp_settings = {
